872b6dc5b3e303c8954230a2760161b4c01cff70
Patrick O'Malley <paddyomalleydesign@gmail.com>
2022-06-21 10:23:02 +0100

    Added DS_Store to .gitignore and updated my pet and test files with constructor functions that add ages and names to my pets and a method which increments age by 1 year each time it is called.

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -5,3 +5,29 @@ describe('constructor', () => {
 		expect(new Pet('Fido')).toBeInstanceOf(Object);
 	});
 });
+
+describe('constructor', () => {
+	it('sets the name property', () => {
+		const pet = new Pet('Fido');
+	
+		expect(pet.name).toEqual('Fido');
+	});	
+});
+
+describe('constructor', () => {
+	it('starts with 0 as an age', () => {
+		const pet = new Pet('Fido');
+
+		expect(pet.age).toEqual(0);
+	});
+});
+
+describe('growUp', () => {
+	it('adds one year to age of pet', () => {
+		const pet = new Pet('Fido');
+
+		pet.growUp();
+
+		expect(pet.age).toEqual(1);
+	});
+});
